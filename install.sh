d52ee9b98f860d36964655a00c016eee2c89d0b4
KingNekoVN <minhgit20@gmail.com>
2025-08-16 09:01:27 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,15 +24,25 @@ mkdir -p /data /novnc /opt/qemu /cloud-init
 # Tải Astro OS
 curl -L https://archive.org/download/astro-os-11-24-h-2/AstroOS%2011%2024H2.iso
 
+# Tạo file IMG
+qemu-img create -f raw /content/disk.img 112G
+
 # Setup noVNC
 curl -L https://github.com/novnc/noVNC/archive/refs/tags/v1.3.0.zip -o /tmp/novnc.zip && \
 unzip /tmp/novnc.zip -d /tmp && \
 mv /tmp/noVNC-1.3.0/* /novnc && \
 rm -rf /tmp/novnc.zip /tmp/noVNC-1.3.0
 
-# Cài đặt cloudflare
-sudo mkdir -p --mode=0755 /usr/share/keyrings && \
-curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null && echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared jammy main' | sudo tee /etc/apt/sources.list.d/cloudflared.list && sudo apt-get update && sudo apt-get install cloudflared
+# Cài đặt ngrok
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+  | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+  && echo "deb https://ngrok-agent.s3.amazonaws.com bookworm main" \
+  | sudo tee /etc/apt/sources.list.d/ngrok.list \
+  && sudo apt update \
+  && sudo apt install ngrok
+# Token cho ngrok
+ngrok config add-authtoken 2xB1jLlRuHFhvqas7ZDdc4K8G23_4iTdz9zAYYFk3K2YGtiNL
+
 # Tải file start.sh để bắt đầu khởi chạy
 wget -O start.sh https://github.com/Snhvn/Ubuntu123/raw/refs/heads/main/start1.sh
 clear

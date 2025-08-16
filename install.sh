3100bc0a2ce2aa16aaac16736a020177931f26b8
KingNekoVN <minhgit20@gmail.com>
2025-08-16 08:53:01 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+echo "Code By MinhNeko"
+# Tải package bổ sung vào để tải
+apt-get update && apt-get install -y --no-install-recommends \
+    qemu-system-x86 \
+    qemu-utils \
+    sudo \
+    cloud-image-utils \
+    software-properties-common \
+    genisoimage \
+    novnc \
+    websockify \
+    curl \
+    unzip \
+    python3-pip \
+    openssh-client \
+    net-tools \
+    netcat-openbsd \
+    && rm -rf /var/lib/apt/lists/* && apt clean
+
+# Tạo thư mục bên trong noVNC
+mkdir -p /data /novnc /opt/qemu /cloud-init
+
+# Tải Astro OS
+curl -L https://archive.org/download/astro-os-11-24-h-2/AstroOS%2011%2024H2.iso
+
+# Setup noVNC
+curl -L https://github.com/novnc/noVNC/archive/refs/tags/v1.3.0.zip -o /tmp/novnc.zip && \
+unzip /tmp/novnc.zip -d /tmp && \
+mv /tmp/noVNC-1.3.0/* /novnc && \
+rm -rf /tmp/novnc.zip /tmp/noVNC-1.3.0
+
+# Cài đặt cloudflare
+sudo mkdir -p --mode=0755 /usr/share/keyrings && \
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null && echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared jammy main' | sudo tee /etc/apt/sources.list.d/cloudflared.list && sudo apt-get update && sudo apt-get install cloudflared
+# Tải file start.sh để bắt đầu khởi chạy
+wget -O start.sh https://github.com/Snhvn/Ubuntu123/raw/refs/heads/main/start1.sh
+clear
+echo "Đang chạy vui lòng đừng tắt"
+chmod +x start.sh && \
+bash start.sh
